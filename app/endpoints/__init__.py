f035d0b952591093cc42bc0e33cafe0bc9b23d61
mouadeljahiri <63265997+mouadeljahiri@users.noreply.github.com>
2025-05-02 22:30:44 +0100

    Update __init__.py

---
+++ app/endpoints/__init__.py
@@ -0,0 +1,4 @@
+from .generate import router as generate
+from .parse_email import router as parse_email
+from .extract_schedule import router as extract_schedule
+from .extract_milestone import router as extract_milestone
