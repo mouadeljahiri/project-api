4a006fe82116600414de0d2208eeaf12ee946770
mouadeljahiri <63265997+mouadeljahiri@users.noreply.github.com>
2025-05-02 22:31:23 +0100

    Delete app/__init__.py

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generate import router as generate
-from .parse_email import router as parse_email
-from .extract_schedule import router as extract_schedule
-from .extract_milestone import router as extract_milestone
